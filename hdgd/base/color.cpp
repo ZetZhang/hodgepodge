b5d8b0484f240410f0e6e2f9937dae8602d198d7
ZetZhang <13660591402@163.com>
2020-03-18 01:38:43 +0800

    color str pieces

--- hdgd/base/color.cpp
+++ hdgd/base/color.cpp
@@ -3,18 +3,12 @@
 namespace hdgd
 {
 
-static const char* ForeColorCode[static_cast<unsigned char>(Color::ForeColor::NUM_FORE_COLOR)] = {
-    "30m", "31m", "32m", "33m", "34m", "35m", "36m", "37m",
-};
-
-static const char* BackgroundColorCode[static_cast<unsigned char>(Color::BackgroundColor::NUM_BACKGROUND_COLOR)] = {
-    "40;", "41;", "42;", "43;", "44;", "45;", "46;", "47;", ""
-};
 
 } // namespace hdgd
 
 
 using namespace hdgd;
+
 
 // FIXME:需要区分有无底色，无底色5，底色8，结尾4。则每个\r\n需要塞9+1个字节或12+1个字节
 // Need a string piece
@@ -30,8 +24,17 @@ using namespace hdgd;
 //}
 
 const std::pair<const char*, const char*> Color::portion(Color::ForeColor front, Color::BackgroundColor back) {
-    std::string head("\033[");
-    head.append(BackgroundColorCode[static_cast<unsigned int>(back)]);
-    head.append(ForeColorCode[static_cast<unsigned int>(front)]);
-    return std::make_pair(head.c_str(), "\033[0m");
+    std::string *head = new std::string("\033[");
+    head->append(BackgroundColorCode[static_cast<unsigned int>(back)]);
+    head->append(ForeColorCode[static_cast<unsigned int>(front)]);
+    return std::make_pair(head->c_str(), "\033[0m");
+}
+
+std::pair<const char*, const char*>* Color::portionn(Color::ForeColor front, Color::BackgroundColor back) {
+    std::pair<const char*, const char*> *ret = nullptr;
+    std::string *head = new std::string("\033[");
+    //std::string head("\033[");
+    head->append(BackgroundColorCode[static_cast<unsigned int>(back)]);
+    head->append(ForeColorCode[static_cast<unsigned int>(front)]);
+    return new std::pair<const char*, const char*>(head->c_str(), "\033[0m");
 }
--- hdgd/base/color.h
+++ hdgd/base/color.h
@@ -2,10 +2,13 @@
 #define __HDGD_COLOR_H__
 
 #include "hdgd/base/noncopyable.h"
+#include "hdgd/base/StringPiece.h"
 
 #include <cstdio>
 #include <string>
 #include <cstring>
+#include <array>
+#include <vector>
 
 #ifdef DEBUG
 static void pri(const char *buf)
@@ -35,6 +38,7 @@ struct Color
         WHITE,
         NUM_FORE_COLOR,
     };
+
     enum class BackgroundColor {
         BLACK_BACK,
         RED_BACK,
@@ -47,36 +51,130 @@ struct Color
         NONE,
         NUM_BACKGROUND_COLOR,
     };
+
+    // FIXME: one line
     static const std::pair<const char*, const char*> portion(Color::ForeColor front, Color::BackgroundColor back);
+    static /*const*/ std::pair<const char*, const char*>* portionn(Color::ForeColor front, Color::BackgroundColor back);
+
+    struct Portion;
 };
 
-template<size_t LEFT, size_t RIGHT>
-size_t TMP_CMP_LEN() {
-    return LEFT > RIGHT ? LEFT : RIGHT;
-}
+// XXX
+static const char* ForeColorCode[static_cast<unsigned char>(Color::ForeColor::NUM_FORE_COLOR)] = {
+    "30m", "31m", "32m", "33m", "34m", "35m", "36m", "37m",
+};
+
+static const char* BackgroundColorCode[static_cast<unsigned char>(Color::BackgroundColor::NUM_BACKGROUND_COLOR)] = {
+    "40;", "41;", "42;", "43;", "44;", "45;", "46;", "47;", ""
+};
+
+struct Color::Portion {
+    Portion(Color::ForeColor fore, Color::BackgroundColor back) : head(new std::string("\033[")), tail(new std::string("\033[0m")){
+        head->append(BackgroundColorCode[static_cast<unsigned int>(back)]);
+        head->append(ForeColorCode[static_cast<unsigned int>(fore)]);
+    }
+
+    const std::pair<const char*, const char*> operator()() {
+        return std::pair<const char*, const char*>(head->c_str(), tail->c_str());
+    }
+
+    ~Portion() {
+        delete head;
+        delete tail;
+    }
+
+    std::string *head;
+    std::string *tail;
+};
 
 // Normal
 template<size_t SIZE>
-class ColorStr
+class ColorStr : NonCopyable
 {
 public:
-    ColorStr();
-    ColorStr(const char *buf);
-    ColorStr(const std::string &buf);
-    ~ColorStr() noexcept;
+    // XXX
 
-    // FIXME: The move constructor did not pass
-    //template<size_t OSIZE>
-    //ColorStr<TMP_CMP_LEN<SIZE, OSIZE>()>(ColorStr<OSIZE> &&x) noexcept;
+    std::vector<uint32_t> __delegateToCountChar(const char &vc) {
+        std::vector<uint32_t> idx;
+        for (uint32_t i = 0; i < SIZE; i++)
+            if (_data[i] == vc)
+                idx.push_back(static_cast<int32_t>(i));
+        return idx;
+    }
 
-    // FIXME: The move constructor did pass
-    template<size_t OSIZE>
-    ColorStr<TMP_CMP_LEN<SIZE, OSIZE>()>& operator=(ColorStr<OSIZE> &&x) noexcept;
+    size_t __delegateToCalcVL(size_t cc) {
+        return cc * (strlen(_pieces->head->c_str()) + strlen(_pieces->tail->c_str()));
+    }
+
+    void __delegateToINITStr(std::string *str, char sp) {
+        std::vector<uint32_t> idx = __delegateToCountChar(sp);
+        StringPiece sPtr(_data);
+        uint32_t o = 0, pace = 0;
+        if (!idx.empty()) {
+            for (uint32_t i = 0; i < idx.size(); i++) {
+                str->append(_pieces->head->c_str());
+                pace = idx[i] - o;
+                o = idx[i] + 1;
+                str->append(std::string(sPtr.data(), pace));
+                sPtr.remove_prefix(pace + 1);
+                str->append(_pieces->tail->c_str());
+                str->push_back('\n');
+            }
+        } else {
+            str->append(_pieces->head->c_str() + sPtr.as_string() + _pieces->tail->c_str());
+        }
+    }
+
+    //ColorStr(const char *buf, Color::ForeColor fore, Color::BackgroundColor back) : _pieces(Color::portionn(fore, back)) {
+    ColorStr(const char *buf, Color::ForeColor fore, Color::BackgroundColor back) : _pieces(new Color::Portion(fore, back)) {
+        ::memmove(_data, buf, SIZE);
+        _convertPtr = new std::string;
+        __delegateToINITStr(_convertPtr, '\n');
+        printf("%ld | %s\n", _convertPtr->size(), (*_convertPtr).c_str());
+    }
+
+    ColorStr(const std::string &buf, Color::ForeColor fore, Color::BackgroundColor back) : _pieces(new Color::Portion(fore, back)) {
+        ::memmove(_data, buf.c_str(), SIZE);
+        _convertPtr = new std::string;
+        __delegateToINITStr(_convertPtr, '\n');
+    }
+
+    // XXX: 没有意义
+    ColorStr(ColorStr &&x) noexcept : _pieces(x._pieces), _convertPtr(x._convertPtr) {
+        std::memmove(_data, x._data, SIZE);
+        ::memset(_data, '\0', SIZE);
+        x._convertPtr = nullptr;
+    }
+
+    ColorStr& operator=(ColorStr &&x) noexcept {
+        if (this != &x) {
+            delete _pieces;
+            _pieces = x._pieces;
+            std::memmove(_data, x._data, SIZE);
+            ::memset(_data, '\0', SIZE);
+            delete _convertPtr;
+            _convertPtr = x._convertPtr;
+            x._convertPtr = nullptr;
+        }
+        return *this;
+    }
+
+    ~ColorStr() noexcept {
+        if (_convertPtr != nullptr) {
+            delete _convertPtr;
+        }
+        ::memset(_data, '\0', SIZE);
+        delete _pieces;
+    }
+
+    const std::string operator()() {
+        return *_convertPtr;
+    }
 
     // FIXME: should be return the string piece
-    const std::string operator()(const char *buf, Color::ForeColor front, Color::BackgroundColor back) {
+    const std::string operator()(const char *buf, Color::ForeColor fore, Color::BackgroundColor back) {
         //const std::pair<const std::string, const std::string> pieces = Color::portion(front, back);
-        const std::pair<const char*, const char*> pieces = Color::portion(front, back);
+        const std::pair<const char*, const char*> pieces = Color::portion(fore, back);
         // FIXME: Parsing the output problem of newline segments
 #ifdef DEBUG
         //std::string tmp(std::string(pieces.first) + buf + std::string(pieces.second));
@@ -101,8 +199,11 @@ public:
         }
     }
 private:
+    Color::Portion *_pieces;
     char _data[SIZE];
-    std::string *_convertStr;
+    std::string *_convertPtr;
+
+    ColorStr() = delete;
 };
 
 // Partial
@@ -112,61 +213,17 @@ class ColorStr<0> : NonCopyable
 {
 public:
     ColorStr() = default;
-    //const std::string operator()(const char *buf, Color::ForeColor front, Color::BackgroundColor back) {
-        //const std::pair<const std::string, const std::string> add = Color::portion(front, back);
-        //return std::string(add.first + buf + add.second);
-    //}
 
     // XXX:只适合单行
-    const std::string operator()(const char *buf, Color::ForeColor front, Color::BackgroundColor back) {
-        const std::pair<const char*, const char*> pieces = Color::portion(front, back);
-#ifdef DEBUG
-        //std::string tmp(std::string(pieces.first) + buf + std::string(pieces.second));
-        //pri(tmp.c_str());
-        //std::string tmp(pieces.first + buf + pieces.second);
-        //pri(tmp.c_str());
-#endif
+    const std::string operator()(const char *buf, Color::ForeColor fore, Color::BackgroundColor back) {
+        const std::pair<const char*, const char*> pieces = Color::portion(fore, back);
         return std::string(std::string(pieces.first) + buf + std::string(pieces.second));
-        //return std::string(pieces.first + buf + pieces.second);
     }
 private:
     ColorStr(ColorStr &&) noexcept = delete;
     ColorStr& operator=(ColorStr &&) noexcept = delete;
 };
 
-template<size_t SIZE>
-ColorStr<SIZE>::ColorStr() : _data() { printf("Color fucking.\n"); }
-
-template<size_t SIZE>
-ColorStr<SIZE>::ColorStr(const char *buf) : _data(buf) {}
-
-template<size_t SIZE>
-ColorStr<SIZE>::ColorStr(const std::string &buf) : _data(buf) {}
-
-template<size_t SIZE>
-ColorStr<SIZE>::~ColorStr() {
-
-}
-
-// FIXME: 类型模板参数的固化
-//template<size_t SIZE>
-//ColorStr<SIZE>::ColorStr(ColorStr &&x)[> : _data(std::move(x._data)) <]{
-    //_data =
-    //x._data = nullptr;
-//}
-
-// FIXME: has a difference exception specifier
-//template<size_t SIZE>
-//template<size_t OSIZE>
-//ColorStr<TMP_CMP_LEN<SIZE, OSIZE>()>& ColorStr<SIZE>::operator=(ColorStr<OSIZE> &&x) {
-    //size_t cut = TMP_CMP_LEN<SIZE, OSIZE>();
-    //if (_data != x) {
-        //_data = x._data;
-        //_data[cut] = '\0';
-        //x._data = nullptr;
-    //}
-    //return *this;
-//}
 
 
 #define ColorStr(x) static_assert(false, "missing ColorStr var name")
