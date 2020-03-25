98adead0cf9aa6e26791048973d3eb66159e9c84
ZetZhang <13660591402@163.com>
2020-03-25 18:56:07 +0800

    atomic

--- hdgd/base/atomic.h
+++ hdgd/base/atomic.h
@@ -1,87 +1,97 @@
 #ifndef __HDGD_ATOMIC_H__
 #define __HDGD_ATOMIC_H__
 
-#include "hdgd/base/noncopyable.h"
+//#include "hdgd/base/noncopyable.h"
 #include <cstdint>
 
-#if 1
+#if (__cplusplus >= 201103L)
 #include <atomic>
+#define __cpp11
 #endif
 
 namespace hdgd
 {
 
-// C++11 CAS
-
-template<typename T>
-class AtomicIntegerCPP11 : NonCopyable {
-public:
-    AtomicIntegerCPP11() : _value(0) {  }
-
-    T get() {
-        return _value.load();
-    }
-
-    T getAndAdd(T x) {
-        return _value.compare_ex
-    }
-
-private:
-    std::atomic<T> _value;
-};
-
 // Gcc CAS
 template<typename T>
-class AtomicIntegerGCC : NonCopyable {
+class AtomicInteger {
 public:
-    AtomicIntegerGCC() : _value(0) {  }
+    AtomicInteger() : value_(0) {  }
+    AtomicInteger(const AtomicInteger &x) : value_(x.get()) {  }
+    AtomicInteger& operator=(const AtomicInteger &x) { get_and_set(x.get()); return *this; }
 
     T get() {
-        return __sync_val_compare_and_swap(&_value, 0, 0);
+#ifdef __cpp11
+        return std::atomic_load_explicit(&value_, std::memory_order_seq_cst);
+#else
+        return __atomic_load_n(&value_, __ATOMIC_SEQ_CST);
+#endif
     }
 
-    T getAndAdd(T x) {
-        return _sync_fetch_and_add(&_value, x);
+    T get_and_add(T x) {
+#ifdef __cpp11
+        return std::atomic_fetch_add_explicit(&value_, x, std::memory_order_release);
+#else
+        return __atomic_fetch_add(&value_, x, __ATOMIC_RELEASE);
+#endif
     }
 
-    T addAndGet(T x) {
-        return getAndAdd(x) + x;
+    T add_and_get(T x) {
+#ifdef __cpp11
+        return get_and_add(x) + x;
+#else
+        return __atomic_add_fetch(&value_, x, __ATOMIC_RELEASE);
+#endif
     }
 
-    T incrementAndGet() {
-        return addAndGet(1);
+    T increment_and_get() { return add_and_get(1); }
+    T decrement_and_get() { return add_and_get(-1); }
+    T get_and_increment() { return get_and_add(1); }
+    T get_and_decrement() { return get_and_add(-1); }
+
+    void add(T x) { add_and_get(x); }
+    void increment() { increment_and_get(); }
+    void decrement() { decrement_and_get(); }
+
+    T get_and_set(T nVal) {
+#ifdef __cpp11
+        return std::atomic_exchange_explicit(&value_, nVal, std::memory_order_acquire);
+#else
+        return __atomic_exchange_n(&value_, nVal, __ATOMIC_ACQUIRE);
+#endif
     }
 
-    T decrementAndGet() {
-        return addAndGet(-1);
+    /// The ‘__atomic’ builtins can be used with any integral scalar or pointer
+    /// type that is 1, 2, 4, or 8 bytes in length. 16-byte integral types are
+    /// also allowed if ‘__int128’ (see __int128) is supported by the
+    /// architecture.
+    bool is_lock_free() {
+#ifdef __cpp11
+        return std::atomic_is_lock_free(&value_);
+#else
+        return __atomic_is_lock_free(sizeof(T), 0);
+#endif
     }
 
-    void add(T x) {
-        getAndAdd(x);
-    }
-
-    void increment() {
-        incrementAndGet();
-    }
-
-    void decrement() {
-        decrementAndGet();
-    }
-
-    T getAndSet(T nVal) {
-        return __sync_lock_test_and_set(&_value, nVal);
+    bool is_always_lock_free() {
+#ifdef __cpp11
+        return is_lock_free();
+#else
+        return __atomic_always_lock_free(sizeof(T), 0);
+#endif
     }
 
 private:
-    volatile T _value;
+#ifdef __cpp11
+    volatile std::atomic<T> value_;
+#else
+    volatile T value_;
+#endif
 };
-// in gcc >= 4.7 ?
-// __atomic_load_n(&_value, __ATOMIC_SEQ_CST);
-// __atomic_fetch_add(&_value, x, __ATOMIC_SEQ_CST);
-// __atomic_exchange_n(&value, nVal, __ATOMIC_SEQ_CST);
 
-typedef AtomicIntegerGCC<int32_t> AtomicInt32;
-typedef AtomicIntegerGCC<int64_t> AtomicInt64;
+typedef AtomicInteger<int32_t> AtomicInt32;
+typedef AtomicInteger<int64_t> AtomicInt64;
+//typedef AtomicInteger<__int128> AtomicInt128;
 
 } // namespace hdgd
 
